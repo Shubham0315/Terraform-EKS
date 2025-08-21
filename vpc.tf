c52387907cc70662220d0214354d93875f614421
Shubham Haudge <105341138+Shubham0315@users.noreply.github.com>
2025-08-21 00:24:04 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,22 @@
 provider "aws" {
   region = var.aws_region
 }
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "shubham-eks-${random_string.suffix.result}"
+}
+
+resource "random string" "suffix" {
+  length = 8
+  special = false
+}
+
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.7.0"
+
+  
+}
